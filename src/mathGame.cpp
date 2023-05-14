3c22b8109aaee105e5d83ab7907dd04dd4d89a26
Vladyslav Zhadan <zhadavla@fit.cvut.cz>
2023-05-14 14:29:49 +0200

    comment math game

--- src/mathGame.cpp
+++ src/mathGame.cpp
@@ -16,6 +16,7 @@ void mathGameStart() {
   EsploraTFT.stroke(255, 255, 0);
 }
 
+// defines variables to work with
 int max_num = MAX_NUM_START;
 int mathSelectedOption = MATH_SEL1;
 int answer, num1, num2;
@@ -23,13 +24,12 @@ char op;
 int options[3];
 int correct_answer_pos;
 int correct_answers;
-//int counter = 0;
 int numOfCorrectAnswers = -1;
 int MATH_MAX_SCORE = 0;
+
+
 void mathGameMain() {
-
-
-  if (!Esplora.readButton(SWITCH_LEFT)) {
+  if (!Esplora.readButton(SWITCH_LEFT)) { // if switch left return to the main menu
     menuState = MATH_SEL;
     prevGameState = SELECTION;
     menuInit();
@@ -37,9 +37,7 @@ void mathGameMain() {
     while (!Esplora.readButton(SWITCH_LEFT));
   }
 
-  if (!Esplora.readButton(SWITCH_RIGHT)) {
-    Serial.println("MAXXXXXXXXXXXXXXXX: " + String(max_num));
-
+  if (!Esplora.readButton(SWITCH_RIGHT)) { // select option
     mathUserInput();
     generateRandomEquation();
     displayMathMenu();
@@ -47,47 +45,39 @@ void mathGameMain() {
     while (!Esplora.readButton(SWITCH_RIGHT));
   }
 
-  if (!Esplora.readButton(SWITCH_UP)) {
-
+  if (!Esplora.readButton(SWITCH_UP)) { // move up
     mathSwitchUp();
     displayAnswers();
-
     delay(100);
     while (!Esplora.readButton(SWITCH_UP));
   }
 
-  if (!Esplora.readButton(SWITCH_DOWN)) {
-
+  if (!Esplora.readButton(SWITCH_DOWN)) { // move down
     mathSwitchDown();
     displayAnswers();
-
-
     delay(100);
     while (!Esplora.readButton(SWITCH_DOWN));
   }
-
 }
 
 void generateRandomEquation() {
-
-  op = "+-*"[rand() % 3];
+  op = "+-*"[rand() % 3]; // choose operation
+  // generate random equation
   if (op == '*') {
     if (max_num > MAX_NUM_INC) {
       num1 = rand() % (max_num - MAX_NUM_INC) + MIN_NUM;
       num2 = rand() % (max_num - MAX_NUM_INC) + MIN_NUM;
     }
-
   } else {
     num1 = rand() % max_num + MIN_NUM;
     num2 = rand() % max_num + MIN_NUM;
   }
+
   if (op == '+') {
     answer = num1 + num2;
-  }
-  else if (op == '-') {
+  } else if (op == '-') {
     answer = num1 - num2;
-  }
-  else if (op == '*') {
+  } else if (op == '*') {
     answer = num1 * num2;
   }
 
@@ -95,62 +85,32 @@ void generateRandomEquation() {
   int min_option = pow(10, num_digits - 1);
   int max_option = pow(10, num_digits) - 1;
 
-  correct_answer_pos = rand() % NUM_OPTIONS;
-  Serial.println("correct_answer_pos: " + String(correct_answer_pos));
+  correct_answer_pos = rand() % NUM_OPTIONS; // generate position of the correct answer
   for (int i = 0; i < NUM_OPTIONS; i++) {
     if (i == correct_answer_pos) {
       options[i] = answer;
-    }
-    else {
+    } else {
       int option = 0;
-      do {
+      do { // while all options equal generate new
         option = rand() % (max_option - min_option + 1) + min_option;
-        options[i] = rand() % (max_option - min_option + 1) + min_option;
-        if (answer < 0)
-          options[i] *= -1;
-        Serial.println(option);
+        if (answer < 0) options[i] *= -1;
       } while ((options[0] == option || options[1] == option || options[2] == option) || option == 0);
       options[i] = option;
     }
   }
 }
 
-int selectedButton() {
-  switch (mathSelectedOption) {
-    case MATH_SEL1:
-      Serial.println("MATH_SEL1");
-      return MATH_SEL1;
-      break;
-    case MATH_SEL2:
-      Serial.println("MATH_SEL2");
-      return MATH_SEL2;
-      break;
-    case MATH_SEL3:
-      Serial.println("MATH_SEL3");
-      return MATH_SEL3;
-      break;
-  }
-}
-
-
 void mathUserInput() {
-  int user_answer = selectedButton();
-
-  Serial.println("user_answer: " + String(user_answer));
-  if (user_answer == correct_answer_pos) {
-    correct_answers++;
-    numOfCorrectAnswers++;
-    if (numOfCorrectAnswers > MATH_MAX_SCORE)
-      MATH_MAX_SCORE = numOfCorrectAnswers;
+  // if correct answer
+  if (mathSelectedOption == correct_answer_pos) {
+    correct_answers++; // increase number of them
+    if (numOfCorrectAnswers > MATH_MAX_SCORE) MATH_MAX_SCORE = numOfCorrectAnswers;
     if (correct_answers == CORRECT_ANSWERS_THRESHOLD) {
       correct_answers = 0;
       max_num += MAX_NUM_INC;
-      if (max_num > MAX_NUM_MAX) {
-        max_num = MAX_NUM_MAX;
-      }
+      if (max_num > MAX_NUM_MAX) max_num = MAX_NUM_MAX;
     }
   } else {
-    numOfCorrectAnswers = 0;
     correct_answers = 0;
     max_num = MAX_NUM_START;
   }
